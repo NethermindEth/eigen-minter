bfa909a261268c67f5a3cfe8669971db587f73e1
AntiD2ta <mtpotro41@gmail.com>
2024-10-25 08:20:31 +0000

    fix: metrics counter reset

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,19 +1,27 @@
 package metrics
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
 type Metrics struct {
 	pushGatewayURL     string
+	pusher             *push.Pusher
 	triggerCounter     prometheus.Counter
 	pressButtonCounter *prometheus.CounterVec
 }
 
-func NewMetrics(pushGatewayURL string) *Metrics {
-	return &Metrics{
+func NewMetrics(pushGatewayURL string) (*Metrics, error) {
+	metrics := &Metrics{
 		pushGatewayURL: pushGatewayURL,
+		pusher:         push.New(pushGatewayURL, "eigen_minter"),
 		triggerCounter: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: "eigen_minter_trigger_total",
 			Help: "Total number of times the application was triggered",
@@ -26,6 +34,56 @@ func NewMetrics(pushGatewayURL string) *Metrics {
 			[]string{"status"},
 		),
 	}
+	if err := metrics.Init(); err != nil {
+		return nil, err
+	}
+	return metrics, nil
+}
+
+func (m *Metrics) Init() error {
+	resp, err := http.Get(m.pushGatewayURL + "/metrics")
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to fetch metrics from pushgateway: %s", resp.Status)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	bodyString := string(body)
+	initializers := []struct {
+		regex   *regexp.Regexp
+		counter prometheus.Counter
+	}{
+		{
+			regex:   regexp.MustCompile(`eigen_minter_press_button_total{.*\bstatus="failed"} (\d+)`),
+			counter: m.pressButtonCounter.WithLabelValues("failed"),
+		},
+		{
+			regex:   regexp.MustCompile(`eigen_minter_press_button_total{.*\bstatus="success"} (\d+)`),
+			counter: m.pressButtonCounter.WithLabelValues("success"),
+		},
+		{
+			regex:   regexp.MustCompile(`eigen_minter_trigger_total (\d+)`),
+			counter: m.triggerCounter,
+		},
+	}
+	for _, initializer := range initializers {
+		matches := initializer.regex.FindStringSubmatch(bodyString)
+		if len(matches) > 0 {
+			value, err := strconv.ParseFloat(matches[1], 64)
+			if err != nil {
+				return err
+			}
+			initializer.counter.Add(value)
+		}
+	}
+
+	return nil
 }
 
 func (m *Metrics) RecordTrigger() {
@@ -41,8 +99,7 @@ func (m *Metrics) RecordPressButtonFailure() {
 }
 
 func (m *Metrics) Push() error {
-	pusher := push.New(m.pushGatewayURL, "eigen_minter")
-	pusher.Collector(m.triggerCounter)
-	pusher.Collector(m.pressButtonCounter)
-	return pusher.Push()
+	m.pusher.Collector(m.triggerCounter)
+	m.pusher.Collector(m.pressButtonCounter)
+	return m.pusher.Push()
 }
